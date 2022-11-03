3be2d9f6255ea1e0c752d25937b4c25ccc325412
Patryk Zajac <patrykzajac.it@gmail.com>
2022-11-03 10:26:40 +0100

    Refactor PageNumber validation
    - Add private constructor to hide public code

--- src/main/java/com/haredev/library/book/controller/validation/patterns/PageNumber.java
+++ src/main/java/com/haredev/library/book/controller/validation/patterns/PageNumber.java
@@ -5,6 +5,8 @@ import io.vavr.control.Validation;
 import static java.util.Objects.isNull;
 
 public final class PageNumber {
+    private PageNumber() { };
+
     public static Validation<String, Integer> validate(Integer pageNumber) {
         return isNull(pageNumber)
                 ? Validation.invalid("Page number cannot be null")
