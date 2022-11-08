30da53c763258a8437fc67bfc665b3c31e18a3bc
Patryk Zajac <patrykzajac.it@gmail.com>
2022-11-08 10:40:16 +0100

    Move unit tests
    - Move all unit test from CategorySpec to BookSpec

--- src/test/groovy/com/haredev/library/book/domain/BookSpec.groovy
+++ src/test/groovy/com/haredev/library/book/domain/BookSpec.groovy
@@ -1,7 +1,9 @@
 package com.haredev.library.book.domain
 
 import com.haredev.library.book.domain.samples.SampleBooks
+import com.haredev.library.book.domain.samples.SampleCategories
 import com.haredev.library.book.dto.BookCreateDto
+import com.haredev.library.book.dto.CategoryCreateDto
 import io.vavr.control.Option
 import spock.lang.Specification
 
@@ -12,6 +14,9 @@ class BookSpec extends Specification {
 
     def final twilight = SampleBooks.createBookSample(0L, "Twilight", "Stephenie Meyer")
     def final django = SampleBooks.createBookSample(1L, "Django", "Quentin Tarantino")
+
+    def final bestseller = SampleCategories.createCategorySample(0L, "Bestsellers", "Best books in library")
+    def final adventure = SampleCategories.createCategorySample(1L, "Adventure", "Adventure books with a lot of amazing histories")
 
     def "System should has no one book"() {
         expect:
@@ -78,5 +83,72 @@ class BookSpec extends Specification {
 
         then: "System should be empty"
         facade.fetchAllBooks(PAGE_SIZE).isEmpty()
+    }
+
+    def "System should has no one category"() {
+        expect:
+        facade.fetchAllCategories(PAGE_SIZE).isEmpty()
+    }
+
+    def "System has one category"() {
+        given: "Should add category to system"
+        facade.addCategory(bestseller)
+
+        when: "System should have one category"
+        Option<CategoryCreateDto> result = facade.findCategoryById(bestseller.categoryId)
+
+        then:
+        result.get() == bestseller
+    }
+
+    def "System has two categories"() {
+        given: "Should add category to system"
+        facade.addCategory(bestseller)
+        facade.addCategory(adventure)
+
+        when: "System should have two categories"
+        Option<CategoryCreateDto> bestseller_result = facade.findCategoryById(bestseller.categoryId)
+        Option<CategoryCreateDto> adventure_result = facade.findCategoryById(adventure.categoryId)
+
+        then:
+        bestseller_result.get() == bestseller
+        adventure_result.get() == adventure
+    }
+
+    def "Should return all categories from system"() {
+        given: "System should add two categories"
+        facade.addCategory(bestseller)
+        facade.addCategory(adventure)
+
+        when: "We ask system to get categories"
+        List<CategoryCreateDto> foundCategories = facade.fetchAllCategories(PAGE_SIZE)
+
+        then: "System should return categories we have added"
+        foundCategories.contains(bestseller)
+        foundCategories.contains(adventure)
+    }
+
+    def "Should remove category from system"() {
+        given: "Should add one category to system"
+        facade.addCategory(bestseller)
+
+        when: "Should remove one category from system"
+        facade.removeCategoryById(bestseller.categoryId)
+
+        then: "System should be empty"
+        facade.fetchAllCategories(PAGE_SIZE).isEmpty()
+    }
+
+    def "Should remove two categories from system"() {
+        given: "Should add two categories to system"
+        facade.addCategory(bestseller)
+        facade.addCategory(adventure)
+
+        when: "Should remove two books from system"
+        facade.removeCategoryById(bestseller.categoryId)
+        facade.removeCategoryById(adventure.categoryId)
+
+        then: "System should be empty"
+        facade.fetchAllCategories(PAGE_SIZE).isEmpty()
     }
 }
