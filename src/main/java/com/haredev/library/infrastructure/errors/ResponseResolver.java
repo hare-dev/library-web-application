1f03efd789ce922b119b8b740e402e9d89ceb031
Patryk Zajac <patrykzajac.it@gmail.com>
2023-02-27 14:00:57 +0100

    ResponseResolver return Optional with errors
    - Add resolve method for option which return HttpStatus with OK or NOT_FOUND

--- src/main/java/com/haredev/library/infrastructure/errors/ResponseResolver.java
+++ src/main/java/com/haredev/library/infrastructure/errors/ResponseResolver.java
@@ -1,6 +1,7 @@
 package com.haredev.library.infrastructure.errors;
 
 import io.vavr.control.Either;
+import io.vavr.control.Option;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,12 @@ public final class ResponseResolver {
         ErrorMessage message = new ErrorMessage(error.getMessage());
         int httpCode = error.getHttpCode();
         return new ResponseEntity<>(message, HttpStatus.valueOf(httpCode));
+    }
+
+    public static <T> ResponseEntity<T> resolve(Option<T> input) {
+        return input
+                .map(x -> new ResponseEntity<>(x, HttpStatus.OK))
+                .getOrElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     public static <T> ResponseEntity<List<T>> resolve(List<T> list) {
