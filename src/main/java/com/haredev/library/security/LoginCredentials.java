d906641d93947e9f0718fb2871133b5e5514c641
Patryk Zajac <patrykzajac.it@gmail.com>
2023-03-10 18:50:01 +0100

    Spring security basic skeleton to run authorization for resources

---
+++ src/main/java/com/haredev/library/security/LoginCredentials.java
@@ -0,0 +1,9 @@
+package com.haredev.library.security;
+
+import lombok.Getter;
+
+@Getter
+public class LoginCredentials {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/haredev/library/security/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.haredev.library.security.controller;
+
+import com.haredev.library.security.LoginCredentials;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class LoginController {
+
+    @PostMapping("/login")
+    public void login(@RequestBody LoginCredentials loginCredentials) {}
+}
