248773d0865e5c8b049dec6bc5fc127ea5fef0e5
Patryk Zajac <patrykzajac.it@gmail.com>
2024-02-01 14:14:00 +0100

    Change implementation of hashCode and equals for uuid
    - Remove @Version annotation with field
    - UUID field set up as final
    - UUID variable set up as UUID type
    - Define @Column annotation with binary representation and updatable with nullable as false

--- src/main/java/com/haredev/library/infrastructure/entity/BaseEntity.java
+++ src/main/java/com/haredev/library/infrastructure/entity/BaseEntity.java
@@ -1,32 +1,25 @@
 package com.haredev.library.infrastructure.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.data.annotation.Version;
 
 import java.io.Serializable;
 import java.util.Objects;
 import java.util.UUID;
 
 @MappedSuperclass
-@Getter
-@Setter
 public abstract class BaseEntity implements Serializable {
-    private String uuid = UUID.randomUUID().toString();
-
-    @Version
-    private Integer version;
+    @Column(columnDefinition = "BINARY(16)", updatable = false, nullable = false)
+    private final UUID uuid = UUID.randomUUID();
 
     @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (!(object instanceof BaseEntity that)) return false;
-        return Objects.equals(uuid, that.uuid);
+    public boolean equals(Object that) {
+       return this == that || that instanceof BaseEntity
+               &&Objects.equals(uuid, ((BaseEntity)that).uuid);
     }
 
     @Override
     public int hashCode() {
-        return uuid != null ? uuid.hashCode() : 0;
+        return Objects.hash(uuid);
     }
 }
