44fa5960e815ddfd8df877197cd27a96bf9adb5a
Patryk Zajac <patrykzajac.it@gmail.com>
2024-02-20 13:00:09 +0100

    Convert User dto's from class to record

--- src/main/java/com/haredev/library/user/domain/dto/UserDetailsDto.java
+++ src/main/java/com/haredev/library/user/domain/dto/UserDetailsDto.java
@@ -1,17 +1,13 @@
 package com.haredev.library.user.domain.dto;
 
 import com.haredev.library.user.domain.api.Authority;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
 import java.util.Set;
 
-@Getter
 @Builder
-@AllArgsConstructor
-public class UserDetailsDto {
-    private final Long id;
-    private final String username;
-    private final Set<Authority> authorities;
-}
+public record UserDetailsDto(
+    Long id,
+    String username,
+    Set<Authority> authorities
+) { }
--- src/main/java/com/haredev/library/user/domain/dto/UserLoginDto.java
+++ src/main/java/com/haredev/library/user/domain/dto/UserLoginDto.java
@@ -1,18 +1,14 @@
 package com.haredev.library.user.domain.dto;
 
 import com.haredev.library.user.domain.api.Authority;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
 import java.util.Set;
 
-@Getter
 @Builder
-@AllArgsConstructor
-public class UserLoginDto {
-    private final Long id;
-    private final String username;
-    private final String password;
-    private final Set<Authority> authorities;
-}
+public record UserLoginDto(
+    Long id,
+    String username,
+    String password,
+    Set<Authority> authorities
+) { }
