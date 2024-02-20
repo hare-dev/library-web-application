87ed1c27df159c5c60f94fb4dfb958dbd6b76856
Patryk Zajac <patrykzajac.it@gmail.com>
2024-02-20 12:46:13 +0100

    Remove @NonNull annotation from doFilterInternal method in TokenAuthenticationFilter

--- src/main/java/com/haredev/library/security/TokenAuthenticationFilter.java
+++ src/main/java/com/haredev/library/security/TokenAuthenticationFilter.java
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,9 +21,9 @@ class TokenAuthenticationFilter extends OncePerRequestFilter {
     private final UserDetailsService userDetailsService;
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
         final String header = request.getHeader("Authorization");
         final String token;
         final String login;
