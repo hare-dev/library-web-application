255537a17ca51428acc3e7167f691fa7eb7daeef
Patryk Zajac <patrykzajac.it@gmail.com>
2024-02-20 12:42:57 +0100

    Convert Authentication request and response from class to record

--- src/main/java/com/haredev/library/security/authentication/input/AuthenticationRequest.java
+++ src/main/java/com/haredev/library/security/authentication/input/AuthenticationRequest.java
@@ -1,11 +1,6 @@
 package com.haredev.library.security.authentication.input;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
-public class AuthenticationRequest {
-    private final String username;
-    private final String password;
-}
+public record AuthenticationRequest(
+    String username,
+    String password
+) { }
--- src/main/java/com/haredev/library/security/authentication/output/AuthenticationResponse.java
+++ src/main/java/com/haredev/library/security/authentication/output/AuthenticationResponse.java
@@ -1,12 +1,8 @@
 package com.haredev.library.security.authentication.output;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 @Builder
-@AllArgsConstructor
-public class AuthenticationResponse {
-    private final String token;
-}
+public record AuthenticationResponse(
+    String token
+) { }
