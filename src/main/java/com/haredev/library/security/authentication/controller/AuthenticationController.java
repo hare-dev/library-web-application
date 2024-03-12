f00797f851b9063f2b2113fd4b679917778f7178
Patryk Zajac <patrykzajac.it@gmail.com>
2024-03-12 18:38:15 +0100

    AuthenticationController return Either error handler or token

--- src/main/java/com/haredev/library/security/authentication/controller/AuthenticationController.java
+++ src/main/java/com/haredev/library/security/authentication/controller/AuthenticationController.java
@@ -1,7 +1,11 @@
 package com.haredev.library.security.authentication.controller;
 
+import com.haredev.library.infrastructure.errors.ResponseResolver;
 import com.haredev.library.security.authentication.AuthenticationFacade;
 import com.haredev.library.security.authentication.controller.input.AuthenticationRequest;
+import com.haredev.library.security.authentication.controller.output.AuthenticationResponse;
+import com.haredev.library.security.authentication.errors.AuthenticationError;
+import io.vavr.control.Either;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +18,8 @@ class AuthenticationController {
     private final AuthenticationFacade authenticateFacade;
 
     @PostMapping("/authenticate")
-    public ResponseEntity authenticate(@RequestBody AuthenticationRequest request) {
-        return ResponseEntity.ok(authenticateFacade.signIn(request));
+    public ResponseEntity<?> authenticate(@RequestBody AuthenticationRequest request) {
+        final Either<AuthenticationError, AuthenticationResponse> response = authenticateFacade.signIn(request);
+        return ResponseResolver.resolve(response);
     }
 }
