9b261a8db03c7c208da4d141be384de8abaffc56
Patryk Zając <patrykzajac.it@gmail.com>
2024-04-10 17:16:29 +0200

    Refactor BookQueryController
    - Add @RequestMapping annotation with started endpoint "/books" for all rest controller methods
    - Rename endpoints to get books by soft cover and by criminal category

--- src/main/java/com/haredev/library/book/controller/BookQueryController.java
+++ src/main/java/com/haredev/library/book/controller/BookQueryController.java
@@ -5,20 +5,22 @@ import com.haredev.library.infrastructure.errors.ResponseResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/books")
 final class BookQueryController {
     private final BookQueryFacade bookQueryFacade;
 
-    @GetMapping("/books/category/criminal")
-    ResponseEntity<?> bookFromCriminalCategory() {
+    @GetMapping
+    ResponseEntity<?> fetchCriminalCategoryBooks() {
         return ResponseResolver.resolve(bookQueryFacade.findAllByCriminalCategory());
     }
 
-    @GetMapping("/books/cover/soft")
-    ResponseEntity<?> bookWithSoftCover() {
+    @GetMapping
+    ResponseEntity<?> fetchSoftCoverBooks() {
         return ResponseResolver.resolve(bookQueryFacade.findAllBySoftCover());
     }
 }
