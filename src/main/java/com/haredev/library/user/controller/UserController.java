f3fdb9105ef135ad990f07fd36f2739c28f8df7d
Patryk Zając <patrykzajac.it@gmail.com>
2024-04-11 13:56:53 +0200

    Change return type from methods in UserController class with var construction and set up for all controller started endpoint path with request mapping annotation

--- src/main/java/com/haredev/library/user/controller/UserController.java
+++ src/main/java/com/haredev/library/user/controller/UserController.java
@@ -3,60 +3,56 @@ package com.haredev.library.user.controller;
 import com.haredev.library.infrastructure.errors.ResponseResolver;
 import com.haredev.library.user.controller.input.RegistrationRequest;
 import com.haredev.library.user.domain.UserFacade;
-import com.haredev.library.user.domain.api.error.UserError;
-import com.haredev.library.user.domain.dto.UserPublicDetailsDto;
-import io.vavr.control.Either;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 class UserController {
     private final UserFacade userFacade;
 
-    @PostMapping("/registration/user")
+    @PostMapping("/registration")
     public ResponseEntity<?> registerAsUser(@RequestBody @Valid final RegistrationRequest request) {
         return ResponseResolver.resolve(userFacade.registerAsUser(request));
     }
 
-    @GetMapping("/users")
+    @GetMapping
     ResponseEntity<?> fetchAllUsers(@RequestParam(required = false) final Integer page) {
         int pageNumber = page != null && page >= 0 ? page : 0;
-        List<UserPublicDetailsDto> response = userFacade.fetchAllUsers(pageNumber);
+        var response = userFacade.fetchAllUsers(pageNumber);
         return ResponseResolver.resolve(response);
     }
 
-    @GetMapping("/users/{id}")
+    @GetMapping("/{id}")
     ResponseEntity<?> findUserById(@PathVariable final Long id) {
-        Either<UserError, UserPublicDetailsDto> response = userFacade.findUserById(id);
+        var response = userFacade.findUserById(id);
         return ResponseResolver.resolve(response);
     }
 
-    @DeleteMapping("/users/{id}")
+    @DeleteMapping("/{id}")
     ResponseEntity<?> removeUserById(@PathVariable final Long id) {
         userFacade.removeUserById(id);
         return ResponseEntity.noContent().build();
     }
 
-    @PutMapping("/users/promote/{id}")
+    @PutMapping("/promote/{id}")
     ResponseEntity<?> promoteToAdmin(@PathVariable final Long id) {
-        Either<UserError, UserPublicDetailsDto> response = userFacade.promoteToAdmin(id);
+        var response = userFacade.promoteToAdmin(id);
         return ResponseResolver.resolve(response);
     }
 
-    @PutMapping("/users/{id}")
+    @PutMapping("/{id}")
     ResponseEntity<?> changeUsername(@PathVariable final Long id, @RequestParam final String newUsername) {
-        Either<UserError, UserPublicDetailsDto> response = userFacade.changeUsername(id, newUsername);
+        var response = userFacade.changeUsername(id, newUsername);
         return ResponseResolver.resolve(response);
     }
 
-    @PutMapping("users/registration/confirmation")
+    @PutMapping("/registration/confirmation")
     ResponseEntity<?> confirmRegistration(@RequestParam final String token) {
-        Either<UserError, UserPublicDetailsDto> response = userFacade.confirmRegistration(token);
+        var response = userFacade.confirmRegistration(token);
         return ResponseResolver.resolve(response);
     }
 }
